adb484bf4c27452bd6f625e5393a4c6320d7720e
Takanori Sono <takanori.sono@gmail.com>
2020-01-06 22:12:03 -0800

    Add React dependencies and render first component to DOM

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,1 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+ 
+class MyComponent extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+ 
+ReactDOM.render(<MyComponent />, document.getElementById('root'));
+
 console.log('App.jsx file has been run')
