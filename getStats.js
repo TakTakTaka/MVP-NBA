7595428556f6d8812b389121dd3ac8b01e730274
Takanori Sono <takanori.sono@gmail.com>
2020-01-07 21:26:19 -0800

    Pull stats from balldontlie API using player search and ID

--- getStats.js
+++ getStats.js
@@ -15,12 +15,23 @@ function playerSearch(player) {
         if(err){throw err}
         //assuming name is correct
         var playerId = JSON.parse(body).data[0].id;
-        url = `https://www.balldontlie.io/api/v1/stats?player_ids[]=132&start_date=2020-01-01`
-        // request()
+        var d = new Date
+        d.setDate(d.getDate()-7).toISOString
+        var weekDate = JSON.stringify(d).slice(0,11);
+
+        url = `https://www.balldontlie.io/api/v1/stats?player_ids[]=${playerId}&start_date=${weekDate}`
+        request(url, (err, res, body) => {
+            if(err){throw err}
+            var stats = JSON.parse(body)
+            console.log(`Player selected ${player} has ID ${playerId} and the following stats:`, stats )
+            
+            return stats
+        })
+        
             
     })
 }
 
 // https://www.balldontlie.io/api/v1/stats?player_ids[]=132&start_date=2020-01-01
 
-// playerSearch('luka doncic')
+playerSearch('luka doncic')
